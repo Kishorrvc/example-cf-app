cbb0ce728d5cb6826aaeeb935cd919ded65ebab6
Kishor Kumar S <kishor.kumar.s@sap.com>
2025-05-28 21:28:15 +0530

    remove app.run params

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from a single-file CF app with proper dependencies!"
+    return "<<Hello from a single-file CF app with proper dependencies!>>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080)
+    app.run()
